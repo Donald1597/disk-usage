a2505717d758ba7ff186de1846577f95fd71909b
Donald APOVO <apovodonald@gmail.com>
2024-07-24 02:00:10 +0100

    review

--- src/config/disk-usage.php
+++ src/config/disk-usage.php
@@ -2,8 +2,8 @@
 
 return [
     'quota_threshold' => [
-        'percentage' => env('DISK_USAGE_THRESHOLD_PERCENTAGE', 80), // Valeur par défaut: 80%
-        'absolute' => env('DISK_USAGE_THRESHOLD_ABSOLUTE', 10 * 1024 * 1024 * 1024), // Valeur par défaut: 10 GB
+        'percentage' => env('DISK_USAGE_THRESHOLD_PERCENTAGE', 80), // default value: 80%
+        'absolute' => env('DISK_USAGE_THRESHOLD_ABSOLUTE', 10 * 1024 * 1024 * 1024), // default value: 10 GB
         'notification_email' => env('DISK_USAGE_NOTIFICATION_EMAIL', 'test@example.com'),
 
     ],
